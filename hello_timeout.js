e1bfcc5e5c88f71788d032d04993eaf78b402a9b
Shruti Shetty <shetty_14august@yahoo.com>
2020-09-21 15:54:28 +0000

    Adding the setTimeout Functionality

---
+++ hello_timeout.js
@@ -0,0 +1,19 @@
+//CHallenge 1
+setTimeout(()=>{
+console.log('4311o th3r3 w0r1d');
+
+},3000);
+
+//Challenge 2
+
+setTimeout(()=>{
+  console.log('4311o');
+},1000);
+
+setTimeout(()=>{
+  console.log('th3r3');
+},1000);
+
+setTimeout(()=>{
+  console.log('w0r1d');
+},1000);
