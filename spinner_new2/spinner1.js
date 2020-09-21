287264bfa788ab1bf34769e0f833506172d0517c
Shruti Shetty <shetty_14august@yahoo.com>
2020-09-21 18:45:57 +0000

    Refactoring Spinner2

--- spinner_new2/spinner1.js
+++ spinner_new2/spinner1.js
@@ -14,3 +14,28 @@ setTimeout(() => {
   // Need to escape the backslash since it's a special character.
   process.stdout.write('\r\\   '); 
 }, 700);
+
+setTimeout(() => {
+  // Need to escape the backslash since it's a special character.
+  process.stdout.write('\r|   '); 
+}, 900);
+
+setTimeout(() => {
+  // Need to escape the backslash since it's a special character.
+  process.stdout.write('\r/   '); 
+}, 1100);
+
+setTimeout(() => {
+  // Need to escape the backslash since it's a special character.
+  process.stdout.write('\r-   '); 
+}, 1300);
+
+setTimeout(() => {
+  // Need to escape the backslash since it's a special character.
+  process.stdout.write('\r\\  '); 
+}, 1500);
+
+setTimeout(() => {
+  // Need to escape the backslash since it's a special character.
+  process.stdout.write('\r|   '); 
+}, 1700);
